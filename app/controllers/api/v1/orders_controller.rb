e33070b511487da92f569c81e937dcd7e3b4acab
Farlaf <slepuhovoleg@gmail.com>
2024-01-02 19:03:30 +0400

    Adds the show action for order

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -8,6 +8,17 @@ module Api
       def index
         render json: OrderSerializer.new(current_user.orders).serializable_hash
       end
+
+      def show
+        order = current_user.orders.find(params[:id])
+
+        if order
+          options = { include: [:products] }
+          render json: OrderSerializer.new(order, options).serializable_hash
+        else
+          head :not_found
+        end
+      end
     end
   end
 end
--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -25,6 +25,19 @@ module Api
         json_response = response.parsed_body
         assert_equal @order.user.orders.count, json_response['data'].count
       end
+
+      test 'should show order' do
+        get api_v1_order_url(@order),
+            headers: {
+              Authorization: JsonWebToken.encode(user_id: @order.user_id)
+            },
+            as: :json
+        assert_response :success
+
+        json_response = response.parsed_body
+        include_product_attr = json_response['included'][0]['attributes']
+        assert_equal @order.products.first.title, include_product_attr['title']
+      end
     end
   end
 end
